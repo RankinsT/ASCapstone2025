2693164db24b7bc13b48660f7e4226e7efeaa2bf
RankinsT <122284235+RankinsT@users.noreply.github.com>
2025-07-30 10:47:20 -0400

    Initial homepage header
    Very basic changes due to a disabled autosave incident.

--- homeViewSections/header.php
+++ homeViewSections/header.php
@@ -1,1 +1,8 @@
-header
+<div class="header-img">
+    <div><img src="" alt="logo"></div>
+
+    <div class="phone-num-btn">
+        <p>Call Now!</p>
+        <p>PhoneNumPlaceholder</p>
+    </div>
+</div>
