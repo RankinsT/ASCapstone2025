cca554c053771106f2e0bd6df1839e39ba3fa793
bcmedeiros27 <146851775+bcmedeiros27@users.noreply.github.com>
2025-07-30 09:49:11 -0400

    Update model_admin.php

--- models/model_admin.php
+++ models/model_admin.php
@@ -1,5 +1,6 @@
 <?php
 
+
 include __DIR__ . '/dp.php'; // Include the database connection file
 function login($username, $password) {
     global $db;
@@ -15,37 +16,3 @@ function login($username, $password) {
 
     return false;  // Return false if login failed
 }
-
-//
-/* Code for login insert at top  php
-session_start();
-$_SESSION['isLoggedIn'] = false;
-$_SESSION['username'] = '';
-$error = '';
-
-if(isset($_POST['login'])){
-
-    $username = filter_input(INPUT_POST,'username', FILTER_SANITIZE_STRING);
-    $password = filter_input(INPUT_POST,'password', FILTER_SANITIZE_STRING);
-
-    if(login($username, $password)){
-        $_SESSION['isLogginIn'] = true;
-        $_SESSION['username'] = $username;
-        header('Location: homeView.php'); test location to see if login working 
-    }
-    else{
-        $error = "You did not provide correct creds";
-    }
-/*
-/* Code for error handling Insert into a div in html
-        <?php
-            if ($error != "") {
-        ?>
-        <div class="error"><?php echo $error; ?></div>
-        <?php
-            }
-        ?>
-
-
-}
-//
