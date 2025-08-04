1ab7ec0930c2714aa37c2cb4ed3b0028af9de1b0
RankinsT <122284235+RankinsT@users.noreply.github.com>
2025-08-04 11:15:50 -0400

    Review Include

--- homeViewSections/reviews.php
+++ homeViewSections/reviews.php
@@ -1,1 +1,6 @@
-reviews
+
+<div class="reviews-bg-div">
+
+    <p class="reviews-p">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ex maecenas curae laoreet feugiat eleifend malesuada taciti condimentum ullamcorper ultrices ac consequat. Fermentum quam ultrices metus integer accumsan eleifend dignissim ultrices vitae. Varius dapibus potenti vulputate cubilia ornare interdum augue fusce. Fringilla curabitur curae fames nunc volutpat viverra senectus aenean inceptos vehicula duis per mollis eleifend. Primis dolor justo lacinia lorem cras aliquam fusce. (TESTOMONIAL, REVIEWS)</p>
+
+</div>
