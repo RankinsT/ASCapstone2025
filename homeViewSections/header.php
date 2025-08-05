138923c6b271b86787589e40a2f8e189203caa38
RankinsT <122284235+RankinsT@users.noreply.github.com>
2025-08-05 09:18:03 -0400

    Additional CSS & Cleanup

--- homeViewSections/header.php
+++ homeViewSections/header.php
@@ -1,8 +1,23 @@
-<div class="header-img">
-    <div><img src="" alt="logo"></div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>header</title>
+    <link rel="stylesheet" href="./css/homeStyle.css">
+</head>
+    <body>
+        
+        <div class="header-img">
 
-    <div class="phone-num-btn">
-        <p>Call Now!</p>
-        <p>PhoneNumPlaceholder</p>
-    </div>
-</div>
+            <img src="" alt="logo">
+
+            <div class="phone-num-btn">
+                <p>Call Now!</p>
+                <p>888-888-8888</p>
+            </div>
+            
+        </div>
+
+    </body>
+</html>
