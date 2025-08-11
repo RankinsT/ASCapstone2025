0a27473be7d0bee364075a07f0bbafd8505cc093
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-11 19:08:34 -0400

    Update services.php

--- homeViewSections/services.php
+++ homeViewSections/services.php
@@ -1,1 +1,2 @@
-services
+ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
