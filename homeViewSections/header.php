e41081357861df201919a994273f28adda7c93e2
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-11 19:19:08 -0400

    logo fades out when scrolling down

--- homeViewSections/header.php
+++ homeViewSections/header.php
@@ -9,5 +9,18 @@
         <p><a href="tel:508-717-1249">508-717-1249</a></p>
     </button>
  </div>
-    
-            
+
+<script>
+document.addEventListener('DOMContentLoaded', function() {
+  const logo = document.querySelector('.logo-img');
+  window.addEventListener('scroll', function() {
+    if (window.scrollY > 0) {
+      logo.classList.add('hide-on-scroll');
+    } else {
+      logo.classList.remove('hide-on-scroll');
+    }
+  });
+});
+</script>
+
+
