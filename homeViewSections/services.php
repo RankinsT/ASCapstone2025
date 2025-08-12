5687dac93980149cbb1fdaa92f08791db00e96be
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-12 19:36:27 -0400

    services

--- homeViewSections/services.php
+++ homeViewSections/services.php
@@ -1,6 +1,6 @@
 <div class="col-lg-4">
     <div class="service-outer-div">
-        <div style="background-color: white; padding: 20px;  border-radius: 20px">Service information goes here</div>
+        <div class="service-inner-div">Service information goes here</div>
     </div>
 
     <div class="service-outer-div">
