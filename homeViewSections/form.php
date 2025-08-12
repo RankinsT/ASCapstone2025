06888ddb990578ca2579741627a746b0fca5b1d1
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-12 10:25:21 -0400

    worked on form and comp desc

--- homeViewSections/form.php
+++ homeViewSections/form.php
@@ -1,1 +1,9 @@
-form
+<form action="">
+    <div><input type="text" placeholder="Your Name"></div>
+    <br>
+    <div><input type="email" placeholder="Your Email"></div>
+    <br>
+    <div><textarea placeholder="Your Message"></textarea></div>
+    <br>
+    <div><button type="submit">Send</button></div>
+</form>
