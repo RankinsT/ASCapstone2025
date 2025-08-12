187765b65de14c3ee64fa9c3fd300f9eec07cff8
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-12 18:53:09 -0400

    created carasoul2

--- homeView.php
+++ homeView.php
@@ -7,8 +7,11 @@
 
     <link rel="stylesheet" href="./css/homeStyle.css"> <!-- Link to your home CSS file -->
     <link rel="stylesheet" href="./css/adminStyle.css"> <!-- Link to your admin CSS file -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_back_ios" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
     <script src="https://cdn.tiny.cloud/1/a568yvdjcxzu2hhaiwp0fdun5rd6z9s4d51urh6m0lrpw0eu/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
 </head>
 <body>
@@ -51,7 +54,7 @@
         
         <!-- div /header -->
         <div class="row elements row-1">
-            <div class="reviews col-lg-6 col-md-6 col-sm-12">
+            <div class="reviews col-lg-6">
                 <?php include 'homeViewSections/reviews.php'; ?> <!-- Include the reviews section -->
             </div>
             <!-- div /reviews -->
@@ -63,20 +66,22 @@
         </div>
         
         <div class="row elements row-2">
-            <div class="companyDescription col-lg-4 col-md-6 col-sm-12">
+            <div class="companyDescription col-lg-4">
                 <?php include 'homeViewSections/companyDescription.php'; ?> <!-- Include the company description section -->
             </div>
             <!-- div /companyDescription -->
 
-            <div class="carousel col-lg-7 col-md-8 col-sm-12">
-                <?php include 'homeViewSections/carousel.php'; ?> 
+            <div class="main-carousel-div col-lg-8">
+                <div>
+                    <?php include 'homeViewSections/carousel2.php'; ?>
+                </div>
                 <!-- Include the carousel section -->
             </div>
             <!-- div /carousel -->
         </div>
         
-        <div class="row elements">
-            <div class="services col-lg-12 col-md-12 col-sm-12">
+        <div class="row elements row-3 col-lg-12">
+            <div class="services ">
                 <?php include 'homeViewSections/services.php'; ?> <!-- Include the services section -->
             </div>
             <!-- div /services -->  
---
+++ homeViewSections/carousel2.php
@@ -0,0 +1,55 @@
+<?php
+include __DIR__ . '/../models/db.php'; // Include database connection
+
+try {
+    // Fetch all images from the database
+    $query = "SELECT * FROM bcimage ORDER BY id ASC";
+    $stmt = $db->query($query);
+    $images = $stmt->fetchAll(PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    echo "<h3>Error fetching images: " . $e->getMessage() . "</h3>";
+    $images = [];
+}
+?>
+
+
+<div id="carouselExample" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+        <?php if (!empty($images)): ?>
+            <?php foreach ($images as $index => $image): ?>
+                <div class="carousel-item <?php echo $index === 0 ? 'active' : ''; ?>">
+                    <img src="./bcimage/<?php echo htmlspecialchars($image['filename']); ?>" class="d-block w-100" alt="Slide <?php echo $index + 1; ?>" style="height: 25em; object-fit: cover;">
+                    <div class="carousel-caption d-none d-md-block box" >
+                        <h5>Slide <?php echo $index + 1; ?></h5>
+
+                        <p><?php echo htmlspecialchars($image['description'] ?? 'No description available'); ?></p>
+                    
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <div class="carousel-item active">
+                <div class="d-flex justify-content-center align-items-center" style="height: 25em; background-color:rgb(46, 138, 230);">
+                    <h5>No images available</h5>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
+    <!-- Carousel Indicators -->
+    <div class="carousel-indicators " >
+        <?php foreach ($images as $index => $image): ?>
+            <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="<?php echo $index; ?>" class="<?php echo $index === 0 ? 'active' : ''; ?>" aria-label="Slide <?php echo $index + 1; ?>"></button>
+        <?php endforeach; ?>
+    </div>
+    <!-- Carousel Controls -->
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true" style="filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.74));"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next" style="filter: drop-shadow(0px 0px 3px rgba(0, 0, 0, 0.74));">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
