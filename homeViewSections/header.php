88453cf4137855e75954084dbe2a935e2e3cec12
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-11 20:43:27 -0400

    Update header.php

--- homeViewSections/header.php
+++ homeViewSections/header.php
@@ -1,14 +1,15 @@
+<div></div>
 <div class="header-img">
 
     <img src="images/LogoTemp.png" alt="logo" class="logo-img">
- </div>
+</div>
 
- <div>
+<div>
     <button type="button" class="phone-num-btn">
         <p>Call Now!</p>
         <p><a href="tel:508-717-1249">508-717-1249</a></p>
     </button>
- </div>
+</div>
 
 <script>
 document.addEventListener('DOMContentLoaded', function() {
