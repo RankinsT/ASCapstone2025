ec9be95bc5a18b0be1bb5f01f81e37407b61209c
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-12 20:29:17 -0400

    added location section

--- homeViewSections/locations.php
+++ homeViewSections/locations.php
@@ -1,1 +1,18 @@
-<div>location</div>
+<div class="location-panel">
+    <div class="locations-left">
+        <div class="location-header">
+            <h1>Servicing New England!</h1>
+        </div>
+
+        <div class="states">
+            <div><h2> 📍 Connecticut</h2></div>
+            <br>
+            <div><h2> 📍 Massachusetts</h2></div>
+            <br>
+            <div><h2> 📍 Rhode Island</h2></div>
+        </div>
+    </div>
+    <div class="locations-right">
+        <h2>Location 2</h2>
+    </div>
+</div>
