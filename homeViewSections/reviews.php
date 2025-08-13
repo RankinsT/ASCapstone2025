49064f232d7a48931ab77cf8435b5e112a8f309a
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-13 08:07:20 -0400

    started tiny for reviews

--- homeViewSections/reviews.php
+++ homeViewSections/reviews.php
@@ -1,1 +1,9 @@
-<div></div>
+<div id="reviews-container">
+  <p id="reviews-text" style="margin: 20px;">[Customer Reviews]
+  </p>
+
+  <form action="" id="reviews-form" style="display:none;">
+    <textarea id="reviews-editor">Welcome to TinyMCE!</textarea>
+  </form>
+  <button id="reviews-edit-save-btn" type="button">Edit</button>
+</div>
