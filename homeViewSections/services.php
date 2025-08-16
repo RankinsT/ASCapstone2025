52fec4c18e67ebc499047b23e977ce638263b64e
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-16 13:26:53 -0400

    worked on home

--- homeViewSections/services.php
+++ homeViewSections/services.php
@@ -4,7 +4,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text1" style="margin: 20px;">[Service Description 1]</div>
+                <div id="service-img-text1" style="margin: 20px;"> Re-covering in a variety of colors, repairing small tears, and tightening rails.</div>
                 <form action="" id="service-img-form1" style="display:none;">
                     <textarea name="service-img-editor1" id="service-img-editor1">Welcome to TinyMCE!</textarea>
                 </form>
@@ -14,7 +14,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text2" style="margin: 20px;">[Service Description 2]</div>
+                <div id="service-img-text2" style="margin: 20px;">Shifting your table to a new room or basement without leaving the property.</div>
                 <form action="" id="service-img-form2" style="display:none;">
                     <textarea name="service-img-editor2" id="service-img-editor2">Welcome to TinyMCE!</textarea>
                 </form>
@@ -24,7 +24,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text3" style="margin: 20px;">[Service Description 3]</div>
+                <div id="service-img-text3" style="margin: 20px;">Disassembly, transport, and reassembly of all table sizes, including slate tables.</div>
                 <form action="" id="service-img-form3" style="display:none;">
                     <textarea name="service-img-editor3" id="service-img-editor3">Welcome to TinyMCE!</textarea>
                 </form>
@@ -36,7 +36,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text4" style="margin: 20px;">[Service Description 4]</div>
+                <div id="service-img-text4" style="margin: 20px;">Disassembly, transport, and reassembly of all table sizes, including slate tables.</div>
                 <form action="" id="service-img-form4" style="display:none;">
                     <textarea name="service-img-editor4" id="service-img-editor4">Welcome to TinyMCE!</textarea>
                 </form>
@@ -46,7 +46,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text5" style="margin: 20px;">[Service Description 5]</div>
+                <div id="service-img-text5" style="margin: 20px;">Handling cracked or damaged slate </div>
                 <form action="" id="service-img-form5" style="display:none;">
                     <textarea name="service-img-editor5" id="service-img-editor5">Welcome to TinyMCE!</textarea>
                 </form>
@@ -56,7 +56,7 @@
         <div class="service-outer-div">
             <div class="service-inner-div">
                 <div class="service-img"></div>
-                <div id="service-img-text6" style="margin: 20px;">[Service Description 6]</div>
+                <div id="service-img-text6" style="margin: 20px;">Disassembly, transport, and reassembly of all table sizes, including slate tables.</div>
                 <form action="" id="service-img-form6" style="display:none;">
                     <textarea name="service-img-editor6" id="service-img-editor6">Welcome to TinyMCE!</textarea>
                 </form>
