bdaf3684cb32d45f44930fe814f652fcee488da4
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-18 09:03:13 -0400

    updated form

--- homeViewSections/form.php
+++ homeViewSections/form.php
@@ -1,29 +1,29 @@
-<div>
-    <div><h2>Free Quote</h2></div>
-    <div><p>Please fill out the form below to request a free quote.</p></div>
+<div class="quote-container">
+    <div class="quote-header">
+        <div><h2>Free Quote</h2></div>
+        <div><p>Please fill out the form below to request a free quote.</p></div>
+    </div>
+
+    <div>
+        <div>
+            <input type="text" name="firstName" placeholder="Your First Name*" required>
+        </div>
+        <div>
+            <input type="text" name="lastName" placeholder="Your Last Name*" required>
+        </div>
+        <div>
+            <input type="text" name="email" placeholder="Your Email*" required>
+        </div>
+        <div>
+            <input type="text" name="phone" placeholder="Your Phone">
+        </div>
+        <div>
+            <div><button>Previous</button></div>
+            <div><button>Next</button></div>
+        </div>
+        <div>
+            completion bar here
+        </div>
+    </div>
+
 </div>
-
-<div>
-    <div>
-        <input type="text" name="firstName" placeholder="Your First Name*" required>
-    </div>
-    <div>
-        <input type="text" name="lastName" placeholder="Your Last Name*" required>
-    </div>
-    <div>
-        <input type="text" name="email" placeholder="Your Email*" required>
-    </div>
-    <div>
-        <input type="text" name="phone" placeholder="Your Phone">
-    </div>
-    <div>
-        <div>previous</div>
-        <div>next</div>
-    </div>
-    <div>
-        completion bar here
-    </div>
-</div>
-
-
-
