83a48300fb1b37ad4f06205a03a8b58656a2a117
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-18 08:49:32 -0400

    started form

--- homeViewSections/form.php
+++ homeViewSections/form.php
@@ -1,11 +1,28 @@
 <div>
-   <input type="text" name="name" placeholder="Your Name">
-    <br>
-    <input type="email" name="email" placeholder="Your Email">
-    <br>
-    <textarea name="message" placeholder="Your Message"></textarea>
-    <br>
-    <button type="submit">Send</button>
+    <div><h2>Free Quote</h2></div>
+    <div><p>Please fill out the form below to request a free quote.</p></div>
+</div>
+
+<div>
+    <div>
+        <input type="text" name="firstName" placeholder="Your First Name*" required>
+    </div>
+    <div>
+        <input type="text" name="lastName" placeholder="Your Last Name*" required>
+    </div>
+    <div>
+        <input type="text" name="email" placeholder="Your Email*" required>
+    </div>
+    <div>
+        <input type="text" name="phone" placeholder="Your Phone">
+    </div>
+    <div>
+        <div>previous</div>
+        <div>next</div>
+    </div>
+    <div>
+        completion bar here
+    </div>
 </div>
 
 
