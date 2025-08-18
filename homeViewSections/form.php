5059510e05f2308b95a0a136f255059ad16be797
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-18 09:46:14 -0400

    Update form.php

--- homeViewSections/form.php
+++ homeViewSections/form.php
@@ -59,3 +59,41 @@
         <div>Progression bar</div>
     </div>
 </div>
+
+<div class="quote-container quote-hide">
+    <div>
+        <div><h2>Free Quote</h2></div>
+        <div><p>Get a free quote today!</p></div>
+    </div>
+    <div class="customer-info">
+        <div>
+            <div></div>
+        </div>
+                    <div>
+                        <label for="notes">Notes:</label>
+                        <select name="notes" id="notes" style="width:100%;">
+                            <option value="">Select a note...</option>
+                            <option value="Need expedited service">Need expedited service</option>
+                            <option value="Request weekend move">Request weekend move</option>
+                            <option value="Have stairs">Have stairs</option>
+                            <option value="Need table assembly">Need table assembly</option>
+                            <option value="Other">Other (please specify in comments)</option>
+                        </select>
+                    </div>
+        <div>
+            <div></div>
+        </div>
+        
+    </div>
+    <div class="form-btns-container">
+        <div>
+            <button>previous</button>
+        </div>
+        <div>
+            <button>next</button>
+        </div>
+    </div>
+    <div class="progression-bar">
+        <div>Progression bar</div>
+    </div>
+</div>
