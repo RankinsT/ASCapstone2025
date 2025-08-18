8f37c7ca5d9f50aa479a31f3be4db408d2303858
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-18 09:39:30 -0400

    worked on form

--- homeViewSections/form.php
+++ homeViewSections/form.php
@@ -1,9 +1,9 @@
-<div class="quote-container">
+<div class="quote-container quote-show">
     <div>
         <div><h2>Free Quote</h2></div>
         <div><p>Get a free quote for your project today!</p></div>
     </div>
-    <div class="customer-info1">
+    <div class="customer-info">
         <div>
             <div><input type="text" name="firstName" placeholder="First Name*" required></div>
         </div>
@@ -12,6 +12,40 @@
         </div>
         <div><input type="text" name="email" placeholder="Email*" required></div>
         <div><input type="text" name="phone" placeholder="Phone"></div>
+    </div>
+    <div class="form-btns-container">
+        <div>
+            <!-- <button>previous</button> -->
+        </div>
+        <div>
+            <button>next</button>
+        </div>
+    </div>
+    <div class="progression-bar">
+        <div>Progression bar</div>
+    </div>
+</div>
+
+<div class="quote-container quote-hide">
+    <div>
+        <div><h2>Free Quote</h2></div>
+        <div><p>Get a free quote for your project today!</p></div>
+    </div>
+    <div class="customer-info">
+        <div>
+            <div><input type="text" name="street" placeholder="Street Address*" required></div>
+        </div>
+        <div><input type="text" name="unit" placeholder="Unit/Apt"></div>
+        <div>
+            <div><input type="text" name="city" placeholder="City*" required></div>
+        </div>
+        <div>
+            <div><input type="text" name="state" placeholder="State*" required></div>
+        </div>
+        <div>
+            <div><input type="text" name="zip" placeholder="Zip Code*" required></div>
+        </div>
+        
     </div>
     <div class="form-btns-container">
         <div>
