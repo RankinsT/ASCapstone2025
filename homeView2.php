16e711b30889cced73b9680b101a399e158d6651
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-20 08:08:31 -0400

    started font

--- homeView2.php
+++ homeView2.php
@@ -7,6 +7,9 @@
 
     <link rel="stylesheet" href="./css/homeStyle2.css"> <!-- Link to your home CSS file -->
     <link rel="stylesheet" href="./css/adminStyle.css"> <!-- Link to your admin CSS file -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_back_ios" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
