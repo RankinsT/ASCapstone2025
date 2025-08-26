d2d2173e659fd372bcfff280443bfd5695f9419d
HToy91 <jfvargasfigueroa@email.neit.edu>
2025-08-26 10:30:03 -0400

    Update privacyPolicy.php

--- privacyPolicy.php
+++ privacyPolicy.php
@@ -4,7 +4,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Privacy Policy</title>
-	<link rel="stylesheet" href="css/homeStyle.css">
+	<link rel="stylesheet" href="./css/homeStyle2.css"> <!-- Link to your home CSS file -->
+    <link rel="stylesheet" href="./css/adminStyle.css"> <!-- Link to your admin CSS file -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0&icon_names=arrow_back_ios" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <script src="https://cdn.tiny.cloud/1/a568yvdjcxzu2hhaiwp0fdun5rd6z9s4d51urh6m0lrpw0eu/tinymce/8/tinymce.min.js" referrerpolicy="origin" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
+    <style>
+        p {
+            color: white;
+        }
+    </style>
 </head>
 <body>
 	<div class="container" style="max-width:800px;margin:auto;padding:2em;">
@@ -27,5 +46,27 @@
 		<h2>Contact Us</h2>
 		<p>If you have any questions about this Privacy Policy, please contact us at info@example.com.</p>
 	</div>
+
+	<!-- footer from homeView2 -->
+	<div class="home-footer">
+        <hr style="border: 5px solid white">
+        <hr style="border: 5px solid white">
+        <div class="contact-info">
+            <div class="header-img">
+                <img src="images/LogoTemp.png" alt="logo" class="logo-img">
+            </div>
+            <div><a href="tel:+1234567890"><span style="color: white; font-weight: bold;">CONTACT: </span>234.567.8901</a></div>
+            <div><a href="loginView.php">Admin Login</a></div>
+        </div>
+        <hr style="border: 5px solid white">
+        <div class="footer-content">
+            <p>&copy; 2025 NORCAL Pool Movers. All rights reserved.</p>
+            <!-- link to top -->
+            <div class="back-to-top">
+                <a href="#top" style="color: white; text-decoration: underline;">Back to Top</a>
+            </div>
+        </div>
+    </div>
+	<!-- end footer -->
 </body>
 </html>
